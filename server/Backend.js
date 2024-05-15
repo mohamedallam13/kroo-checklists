33fed51ddc1ffe182bc104cbb71da0d2665dae79
mohamedallam <mh.allam@yahoo.com>
2024-05-15 23:30:46 +0300

    Add Backend functions

--- server/Backend.js
+++ server/Backend.js
@@ -2,21 +2,35 @@
     root.BACKEND = factory();
 })(this, function () {
 
+    const { SHEETS, Toolkit } = KROOLibraries
+    const { timestampCreate } = Toolkit
+    const { getSSMan, parseSheet, writeToSheet, createWriteArr } = SHEETS
+
+
     const SSID = "11CVLmJDnCj2CdrcGSv0S3llrwtqo4PlpaZfC74jZK8E"
 
     const SHEET_NAMES = {
         "startup-checklist-form": "Startup Records"
     }
 
-    function submitChecklist(data) {
-        const { formId } = data
-        const sheet = getSheet(formId)
+    const sheetParseConfig = {
+        headerRow: 2
+    }
 
+    function submitChecklist(data) {
+        const timestamp = timestampCreate(undefined, "MM/DD/YYYY HH:mm:ss")
+        const { formId } = data
+        const sheetObj = getSheetObj(formId)
+        const writeArray = createWriteArr({ timestamp, ...data }, sheetObj)
+        writeToSheet(writeArray, sheetObj)
         return true
     }
 
-    function getSheet(formId) {
-
+    function getSheetObj(formId) {
+        const sheetName = SHEET_NAMES[formId]
+        const ssMan = getSSMan(SSID, sheetName)
+        const sheetObj = parseSheet(ssMan, sheetName, sheetParseConfig)
+        return sheetObj
     }
 
     return {
